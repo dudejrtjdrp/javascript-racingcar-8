44e13182fc6ed777b3808fccd86ddb900a9d5588
dudejrtjdrp <dudejrtjdrp@naver.com>
2025-10-22 02:12:55 +0900

    feat(race): 라운드 진행 로직 및 Car 객체 관리 추가, winner getter/setter 구현

--- src/Model/Race.js
+++ src/Model/Race.js
@@ -1,1 +1,29 @@
-export default class Race {}
+import { RandomNumberGenerator } from '../Util/randomNumberGenerator.js';
+import Car from './Car.js';
+import OutputHandler from '../view/outputHandler.js';
+
+export default class Race {
+  #round = 0;
+  #winner;
+  #cars = [];
+
+  constructor(carNames) {
+    this.#cars = carNames.map((name) => new Car(name));
+  }
+
+  play(playCount) {
+    while (this.#round < playCount) {
+      this.#round += 1;
+    }
+  }
+
+  calculateWinner() {}
+
+  set winner(winner) {
+    this.#winner = winner;
+  }
+
+  get winner() {
+    return this.#winner;
+  }
+}
